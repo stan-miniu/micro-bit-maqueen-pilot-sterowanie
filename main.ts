1e6f9c86d833412769e1cf6d9e26966f7b4f975d
stan-miniu <121545331+stan-miniu@users.noreply.github.com>
2022-12-28 10:36:31 +0100

    Aktualizacja main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,33 @@
+function ustawLadowarke (pozycjaLadowarki: number) {
+    if (0 < 0) {
+        maqueen.servoRun(maqueen.Servos.S1, 0)
+    } else if (pozycjaLadowarki > 80) {
+        maqueen.servoRun(maqueen.Servos.S1, 80)
+    } else {
+        maqueen.servoRun(maqueen.Servos.S1, pozycjaLadowarki)
+    }
+}
 IR.IR_callbackUser(function (message) {
+    if (message == 24) {
+        ladowarka = 50
+        ustawLadowarke(ladowarka)
+        message = 0
+    }
+    if (message == 16) {
+        ladowarka = 75
+        ustawLadowarke(ladowarka)
+        message = 0
+    }
+    if (message == 242) {
+        ladowarka += -5
+        ustawLadowarke(ladowarka)
+        message = 0
+    }
+    if (message == 243) {
+        ladowarka += 5
+        ustawLadowarke(ladowarka)
+        message = 0
+    }
     if (message == 78) {
         maqueen.motorStop(maqueen.Motors.M1)
         maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 100)
@@ -35,7 +64,9 @@ IR.IR_callbackUser(function (message) {
         maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CCW, 100)
     }
 })
-basic.showString("Pilot jazda v.2")
+let ladowarka = 0
+ladowarka = 50
+basic.showString("Pilot jazda v.4")
 basic.forever(function () {
     if (maqueen.Ultrasonic(PingUnit.Centimeters) <= 10) {
         maqueen.motorStop(maqueen.Motors.All)
